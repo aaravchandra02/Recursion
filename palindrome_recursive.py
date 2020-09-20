99fdadd1a95700ccaeed8f7c0f95a49104e42583
Aarav Chandra <aaravchandra02@gmail.com>
2020-09-20 15:36:04 -0700

    added code

--- palindrome_recursive.py
+++ palindrome_recursive.py
@@ -40,3 +40,18 @@ def is_palindrome(my_string):
 
 Note these solutions do not account for spaces or capitalization in the input!
 """
+
+
+def is_palindrome(s):
+    if len(s) < 2:
+        return True
+    if s[0] != s[-1]:
+        return False
+    return is_palindrome(s[1:-1])
+
+
+# test cases
+print(is_palindrome("abba") == True)
+print(is_palindrome("abcba") == True)
+print(is_palindrome("") == True)
+print(is_palindrome("abcd") == False)
